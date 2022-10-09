3e7425d11f6f696923318ae0e1e78774d9e45645
n-ono <uesugi.4lu.kirari@gmail.com>
2022-10-09 14:44:44 +0900

    初回コミット ( base project )

---
+++ src/main/java/com/example/railway_pricing/RailwayPricingApplication.java
@@ -0,0 +1,13 @@
+package com.example.railway_pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayPricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/railway_pricing/RailwayPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railway_pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
